2258aea810b8626bb5c2dd67e209b9b6ca2deb32
Greeny95 <mick_debeul@hotmail.com>
2015-02-04 11:22:35 +0100

    Sezer Chan

--- src/controller/Bol.java
+++ src/controller/Bol.java
@@ -4,4 +4,10 @@ package controller;
  * Created by Sezer on 4/02/2015.
  */
 public class Bol {
+    public static void main(String[] args) {
+        System.out.println("SEEZER CHAAAAN");
+    }
+    
+
 }
+
